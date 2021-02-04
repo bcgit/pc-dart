f7be295c74efa9d24b7694534f36d5d37946c578
Aditya Kishore <akushwarrior@gmail.com>
2021-02-03 18:56:31 -0800

    add another IGE test from OpenSSL

--- test/modes/ige_test.dart
+++ test/modes/ige_test.dart
@@ -22,4 +22,19 @@ void main() {
     data1,
     ct1
   ]);
+
+  final key2 =
+  createUint8ListFromHexString('5468697320697320616E20696D706C65'.toLowerCase());
+  final iv2 = createUint8ListFromHexString(
+      '6D656E746174696F6E206F6620494745206D6F646520666F72204F70656E5353'.toLowerCase());
+  final params2 = ParametersWithIV(KeyParameter(key2), iv2);
+
+  final data2 = String.fromCharCodes(createUint8ListFromHexString(
+      '99706487A1CDE613BC6DE0B6F24B1C7AA448C8B9C3403E3467A8CAD89340F53B'));
+  final ct2 = '4C2E204C6574277320686F70652042656E20676F74206974207269676874210A'.toLowerCase();
+
+  runBlockCipherTests(BlockCipher('AES/IGE'), params2, [
+    data2,
+    ct2
+  ]);
 }
